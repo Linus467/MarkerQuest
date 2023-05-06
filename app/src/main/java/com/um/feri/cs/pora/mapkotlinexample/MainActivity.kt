84c1cd8c954cc6c02326675fd98f7f5885a13dc8
Linus467 <78028034+Linus467@users.noreply.github.com>
2023-05-06 15:12:14 +0200

    Map Added

--- app/src/main/java/com/um/feri/cs/pora/mapkotlinexample/MainActivity.kt
+++ app/src/main/java/com/um/feri/cs/pora/mapkotlinexample/MainActivity.kt
@@ -8,10 +8,12 @@ import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
+import android.preference.PreferenceManager
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
@@ -23,7 +25,7 @@ import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.osmdroid.api.IMapController
-import org.osmdroid.config.Configuration
+import org.osmdroid.config.Configuration.*
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -36,277 +38,79 @@ import timber.log.Timber
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var activityResultLauncher: ActivityResultLauncher<Array<String>>
-    private lateinit var fusedLocationClient: FusedLocationProviderClient //https://developer.android.com/training/location/retrieve-current
-    private var lastLoction: Location? = null
-    private var locationCallback: LocationCallback
-    private var locationRequest: LocationRequest
-    private var requestingLocationUpdates = false
-
-    companion object {
-        val REQUEST_CHECK_SETTINGS = 20202
-    }
-
-    init {
-        locationRequest = LocationRequest.create()
-            .apply { //https://stackoverflow.com/questions/66489605/is-constructor-locationrequest-deprecated-in-google-maps-v2
-                interval = 1000 //can be much higher
-                fastestInterval = 500
-                smallestDisplacement = 10f //10m
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-                maxWaitTime = 1000
-            }
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult ?: return
-                for (location in locationResult.locations) {
-                    // Update UI with location data
-                    updateLocation(location) //MY function
-                }
-            }
-        }
-
-        this.activityResultLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { result ->
-            var allAreGranted = true
-            for (b in result.values) {
-                allAreGranted = allAreGranted && b
-            }
-
-            Timber.d("Permissions granted $allAreGranted")
-            if (allAreGranted) {
-                initCheckLocationSettings()
-                //initMap() if settings are ok
-            }
-        }
-    }
-
-    private lateinit var binding: ActivityMainBinding
-    val rnd = Random()
-    lateinit var map: MapView
-    var startPoint: GeoPoint = GeoPoint(46.55951, 15.63970);
-    lateinit var mapController: IMapController
-    var marker: Marker? = null
-    var path1: Polyline? = null
-
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private lateinit var map : MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree()) //Init report type
-        }
-        val br: BroadcastReceiver = LocationProviderChangedReceiver()
-        val filter = IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION)
-        registerReceiver(br, filter)
 
-        //LocalBroadcastManager.getInstance(this).registerReceiver(locationProviderChange)
-        Configuration.getInstance()
-            .load(applicationContext, this.getPreferences(Context.MODE_PRIVATE))
-        binding = ActivityMainBinding.inflate(layoutInflater) //ADD THIS LINE
+        //handle permissions first, before map is created. not depicted here
 
-        map = binding.map
+        //load/initialize the osmdroid configuration, this can be done
+        // This won't work unless you have imported this: org.osmdroid.config.Configuration.*
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, if you abuse osm's
+        //tile servers will get you banned based on this string.
+
+        //inflate and create the map
+        setContentView(R.layout.activity_main)
+
+        map = findViewById<MapView>(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
-        map.setMultiTouchControls(true)
-        mapController = map.controller
-        setContentView(binding.root)
-        val appPerms = arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_NETWORK_STATE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.INTERNET
-        )
-        activityResultLauncher.launch(appPerms)
     }
 
     override fun onResume() {
         super.onResume()
-        binding.map.onResume()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume() //needed for compass, my location overlays, v6.0.0 and up
     }
 
     override fun onPause() {
         super.onPause()
-        if (requestingLocationUpdates) {
-            requestingLocationUpdates = false
-            stopLocationUpdates()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause()  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionsToRequest = ArrayList<String>()
+        var i = 0
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i])
+            i++
         }
-        binding.map.onPause()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        EventBus.getDefault().register(this);
-    }
-
-    override fun onStop() {
-        super.onStop()
-        EventBus.getDefault().unregister(this);
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onMsg(status: MyEventLocationSettingsChange) {
-        if (status.on) {
-            initMap()
-        } else {
-            Timber.i("Stop something")
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE)
         }
     }
 
-    fun initLoaction() { //call in create
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        readLastKnownLocation()
-    }
 
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdates() { //onResume
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.getMainLooper()
-        )
-    }
-
-    private fun stopLocationUpdates() { //onPause
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
-
-    //https://developer.android.com/training/location/retrieve-current
-    @SuppressLint("MissingPermission") //permission are checked before
-    fun readLastKnownLocation() {
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                location?.let { updateLocation(it) }
-            }
-    }
-
-    fun initCheckLocationSettings() {
-        val builder = LocationSettingsRequest.Builder()
-            .addLocationRequest(locationRequest)
-        val client: SettingsClient = LocationServices.getSettingsClient(this)
-        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-        task.addOnSuccessListener { locationSettingsResponse ->
-            Timber.d("Settings Location IS OK")
-            MyEventLocationSettingsChange.globalState = true //default
-            initMap()
-            // All location settings are satisfied. The client can initialize
-            // location requests here.
-            // ...
-        }
-
-        task.addOnFailureListener { exception ->
-            if (exception is ResolvableApiException) {
-                // Location settings are not satisfied, but this can be fixed
-                // by showing the user a dialog.
-                Timber.d("Settings Location addOnFailureListener call settings")
-                try {
-                    // Show the dialog by calling startResolutionForResult(),
-                    // and check the result in onActivityResult().
-                    exception.startResolutionForResult(
-                        this@MainActivity,
-                        REQUEST_CHECK_SETTINGS
-                    )
-                } catch (sendEx: IntentSender.SendIntentException) {
-                    // Ignore the error.
-                    Timber.d("Settings Location sendEx??")
-                }
-            }
-        }
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Timber.d("Settings onActivityResult for $requestCode result $resultCode")
-        if (requestCode == REQUEST_CHECK_SETTINGS) {
-            if (resultCode == RESULT_OK) {
-                initMap()
-            }
+    /*private fun requestPermissionsIfNecessary(String[] permissions) {
+        val permissionsToRequest = ArrayList<String>();
+        permissions.forEach { permission ->
+        if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            permissionsToRequest.add(permission);
         }
     }
-
-    fun updateLocation(newLocation: Location) {
-        lastLoction = newLocation
-        //GUI, MAP TODO
-        binding.tvLat.setText(newLocation.latitude.toString())
-        binding.tvLon.setText(newLocation.longitude.toString())
-        //var currentPoint: GeoPoint = GeoPoint(newLocation.latitude, newLocation.longitude);
-        startPoint.longitude = newLocation.longitude
-        startPoint.latitude = newLocation.latitude
-        mapController.setCenter(startPoint)
-        getPositionMarker().position = startPoint
-        map.invalidate()
-
-    }
-
-
-    fun initMap() {
-        initLoaction()
-        if (!requestingLocationUpdates) {
-            requestingLocationUpdates = true
-            startLocationUpdates()
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
         }
-        mapController.setZoom(18.5)
-        mapController.setCenter(startPoint);
-        map.invalidate()
-    }
-
-
-    private fun getPath(): Polyline { //Singelton
-        if (path1 == null) {
-            path1 = Polyline()
-            path1!!.outlinePaint.color = Color.RED
-            path1!!.outlinePaint.strokeWidth = 10f
-            path1!!.addPoint(startPoint.clone())
-            map.overlayManager.add(path1)
-        }
-        return path1!!
-    }
-
-    private fun getPositionMarker(): Marker { //Singelton
-        if (marker == null) {
-            marker = Marker(map)
-            marker!!.title = "Here I am"
-            marker!!.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-            marker!!.icon = ContextCompat.getDrawable(this, R.drawable.ic_position);
-            map.overlays.add(marker)
-        }
-        return marker!!
-    }
-
-
-    fun onClickDraw1(view: View?) {
-        startPoint.latitude = startPoint.latitude + (rnd.nextDouble() - 0.5) * 0.001
-        mapController.setCenter(startPoint)
-        getPositionMarker().position = startPoint
-        map.invalidate()
-    }
-
-    fun onClickDraw2(view: View?) {
-        startPoint.latitude = startPoint.latitude + (rnd.nextDouble() - 0.5) * 0.001
-        mapController.setCenter(startPoint)
-        val circle = Polygon(map)
-        circle.points = Polygon.pointsAsCircle(startPoint, 40.0 + rnd.nextInt(100))
-        circle.fillPaint.color = 0x32323232 //transparent
-        circle.outlinePaint.color = Color.GREEN
-        circle.outlinePaint.strokeWidth = 2f
-        circle.title = "Area X"
-        map.overlays.add(circle) //Duplicate every time new
-        map.invalidate()
-    }
-
-    fun onClickDraw3(view: View?) {
-        val mCompassOverlay = CompassOverlay(this, InternalCompassOrientationProvider(this), map)
-        mCompassOverlay.enableCompass()
-        map.overlays.add(mCompassOverlay)
-        map.invalidate()
-    }
-
-    fun onClickDraw4(view: View?) {
-        //Polyline path = new Polyline();
-        startPoint.latitude = startPoint.latitude + (rnd.nextDouble() - 0.5) * 0.001
-        startPoint.longitude = startPoint.longitude + (rnd.nextDouble() - 0.5) * 0.001
-        getPath().addPoint(startPoint.clone())
-        map.invalidate()
-    }
+    }*/
 }
-
